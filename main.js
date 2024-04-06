5e40d55cafcc2a4cf925b61c883e02a1dccb67fc
ManahilShah02 <manahilshah429@gmail.com>
2024-04-06 05:30:56 -0700

    push my repo on github

---
+++ main.js
@@ -0,0 +1,41 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+// ARRAY //
+// let fruitchat = ["Banana" , "Apple" , "Orange"];
+// console.log(fruitchat);
+//.push() //
+// fruitchat.push("Melon");
+//.pop() //
+// fruitchat.pop();
+// concat([]) // 
+// fruitchat = fruitchat.concat(["Kiwi" , "Mango"]);
+// console.log(fruitchat);
+// LOOPS //
+//while loop
+// let ramadanDays = 0;
+// while(ramadanDays <= 6){
+//     console.log("Fasting");
+//     console.log("FiveTimesPrayers");
+//     ramadanDays++
+// }
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "Todo",
+            type: "input",
+            message: "What do you want to add in your Todos",
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Are you sure you want to add todos",
+            default: "false",
+        }
+    ]);
+    // console.log(addTask.Todo);
+    todos.push(addTask.Todo);
+    condition = addTask.addmore;
+    console.log(todos);
+}
